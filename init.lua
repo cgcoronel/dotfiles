b4330cf6d74e0834479f4dbf6b7df67575678e60
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-08 15:20:56 -0300

    fix

--- init.lua
+++ init.lua
@@ -43,6 +43,25 @@ vim.g.netrw_banner = 0
 
 -- Configuración específica para el plugin "copilot.nvim"
 vim.cmd('let g:copilot_node_command = "~/node-v18.17.0/bin/node"')
+vim.cmd([[
+let g:copilot_filetypes = {
+  \ 'gitcommit': v:true,
+  \ 'markdown': v:true,
+  \ 'yaml': v:true
+  \ }
+
+function! TerminalMappings()
+  startinsert
+  setlocal nonu nornu
+  tnoremap <buffer> <leader>q <C-\><C-n>:bd!<CR>
+  tnoremap <buffer> <c-h> <C-\><C-n><C-w><C-h>
+  tnoremap <buffer> <c-l> <C-\><C-n><C-w><C-l>
+endfunction
+
+autocmd TermOpen term://*zsh call TerminalMappings()
+autocmd TermOpen term://*npx* call TerminalMappings()
+autocmd BufEnter term://* startinsert
+]])
 
 -- init.lua
 require('keymaps').setup()
