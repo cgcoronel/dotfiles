bb17c3b80ee9daed662bf7b55f72634e5167e952
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-09 19:10:51 -0300

    fix: add colorscheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,12 +1,12 @@
 return {
-    "folke/tokyonight.nvim",
-    lazy = false,
-    priority = 1000, 
-    config = function()
-      require("tokyonight").setup({
-	style = 'night'
-      })
-      vim.cmd.colorscheme('tokyonight-night')
-    end,
+  "folke/tokyonight.nvim",
+  lazy = false,
+  priority = 1000, 
+  config = function()
+    require("tokyonight").setup({
+      style = 'night'
+    })
+    vim.cmd.colorscheme('tokyonight-night')
+  end,
 }
 
