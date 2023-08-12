93f08b0379624fc1fb172c91cf5c8e1bcfed8d97
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-12 14:30:49 -0300

    fix

--- after/plugin/cmp.lua
+++ after/plugin/cmp.lua
@@ -1,15 +1,15 @@
 local cmp = require('cmp')
-local luasnip = require('luasnip')
+-- local luasnip = require('luasnip')
 
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
+--require('luasnip.loaders.from_vscode').lazy_load()
+--luasnip.config.setup {}
 
 cmp.setup {
-    snippet = {
-        expand = function(args)
-            luasnip.lsp_expand(args.body)
-        end,
-    },
+ --   snippet = {
+ --       expand = function(args)
+ --           luasnip.lsp_expand(args.body)
+ --       end,
+ --   },
     mapping = cmp.mapping.preset.insert {
         ['<C-n>'] = cmp.mapping.select_next_item(),
         ['<C-p>'] = cmp.mapping.select_prev_item(),
@@ -31,18 +31,18 @@ cmp.setup {
 --             end
 --         end, { 'i', 's' }),
 
-        ['<S-Tab>'] = cmp.mapping(function(fallback)
-            if cmp.visible() then
-                cmp.select_prev_item()
-            elseif luasnip.locally_jumpable(-1) then
-                luasnip.jump(-1)
-            else
-                fallback()
-            end
-        end, { 'i', 's' }),
+--        ['<S-Tab>'] = cmp.mapping(function(fallback)
+--            if cmp.visible() then
+--                cmp.select_prev_item()
+--            elseif luasnip.locally_jumpable(-1) then
+--                luasnip.jump(-1)
+--            else
+--                fallback()
+--            end
+--        end, { 'i', 's' }),
     },
     sources = {
         { name = 'nvim_lsp' },
-        { name = 'luasnip' },
+    --    { name = 'luasnip' },
     },
 }
--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -6,9 +6,9 @@ return {
     {
         'hrsh7th/nvim-cmp',
         dependencies = {
-            'L3MON4D3/LuaSnip',
-            'saadparwaiz1/cmp_luasnip',
-            'rafamadriz/friendly-snippets',
+            -- 'L3MON4D3/LuaSnip',
+            -- 'saadparwaiz1/cmp_luasnip',
+            -- 'rafamadriz/friendly-snippets',
             'hrsh7th/cmp-nvim-lsp',
         },
     },
