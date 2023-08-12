db44ede805d7a16db5b092e490d7d14e72c31223
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-12 13:30:38 -0300

    fix

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,13 +1,36 @@
 return {
-    'nvim-telescope/telescope.nvim', tag = '0.1.2',
-    event = "VeryLazy",
-    dependencies = { 
-      'nvim-lua/plenary.nvim',
-      'nvim-tree/nvim-web-devicons',
+    {
+       'nvim-telescope/telescope-fzf-native.nvim',
+       build = 'make',
     },
-    keys = {
-      { ">", "<cmd>Telescope live_grep<CR>" },
-      { "<", "<cmd>Telescope git_branches<CR>" },
-      { "<leader>f", "<cmd>Telescope fd<CR>" },
-    },
+    {
+       'nvim-telescope/telescope.nvim', tag = '0.1.2',
+       event = "VeryLazy",
+       dependencies = { 
+         'nvim-lua/plenary.nvim',
+         'nvim-tree/nvim-web-devicons',
+       },
+       keys = {
+         { ">", "<cmd>Telescope live_grep<CR>" },
+         { "<", "<cmd>Telescope git_branches<CR>" },
+         { "<leader>f", "<cmd>Telescope fd<CR>" },
+       },
+       config = function()
+	require('telescope').setup({
+	  extensions = {
+	    fzf = {
+	      fuzzy = true,                    -- false will only do exact matching
+	      override_generic_sorter = true,  -- override the generic sorter
+	      override_file_sorter = true,     -- override the file sorter
+	      case_mode = "ignore_case",       -- or "ignore_case" or "respect_case"
+    		                               -- the default case_mode is "smart_case"
+	    }
+	  }
+	})
+
+	require('telescope').load_extension('fzf')
+      end
+    }
 }
+
+
