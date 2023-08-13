63611a55c1c5589cc0a003bb93bf051e51a28259
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-13 20:03:48 -0300

    fix:

--- lua/plugins/null_ls.lua
+++ lua/plugins/null_ls.lua
@@ -3,6 +3,7 @@ return {
   config = function()
     local null_ls = require("null-ls")
     null_ls.setup({
+      filetypes = { "markdown", "text" },
       sources = {
         null_ls.builtins.formatting.stylua,
         null_ls.builtins.formatting.prettier,
