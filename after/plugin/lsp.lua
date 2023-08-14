2e1f1d08e18cbb3b0ad82506a2f6f4ff594911f6
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-13 22:12:14 -0300

    fix: lsp.lua

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -23,7 +23,6 @@ capabilities = require("cmp_nvim_lsp").default_capabilities(capabilities)
 -- mason
 require("mason").setup()
 require("mason-lspconfig").setup_handlers({
-
   function(server_name)
     require("lspconfig")[server_name].setup({
       on_attach = on_attach,
