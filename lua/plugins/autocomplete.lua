4a362542956ed7dfbe1a6341e15b5ddb07fb7daf
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-20 18:03:43 -0300

    fix

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,15 +1,16 @@
 return {
-  "williamboman/mason.nvim",
-  "williamboman/mason-lspconfig.nvim",
-  "neovim/nvim-lspconfig",
-  "folke/neodev.nvim",
-  {
-    "hrsh7th/nvim-cmp",
-    dependencies = {
-      "L3MON4D3/LuaSnip",
-      "saadparwaiz1/cmp_luasnip",
-      "rafamadriz/friendly-snippets",
-      "hrsh7th/cmp-nvim-lsp",
-    },
-  },
+	"williamboman/mason.nvim",
+	"williamboman/mason-lspconfig.nvim",
+	"neovim/nvim-lspconfig",
+	"folke/neodev.nvim",
+	"jose-elias-alvarez/null-ls.nvim",
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			"L3MON4D3/LuaSnip",
+			"saadparwaiz1/cmp_luasnip",
+			"rafamadriz/friendly-snippets",
+			"hrsh7th/cmp-nvim-lsp",
+		},
+	},
 }
