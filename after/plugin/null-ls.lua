0839bda08d5162cbf58a937b773b1ec568654650
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-20 14:17:48 -0300

    fix: null-ls.lua

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -27,7 +27,7 @@ null_ls.setup({
   debug = false,
   on_attach = on_attach,
   sources = {
- --   null_ls.builtins.formatting.stylua,
+    -- null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
     null_ls.builtins.completion.spell,
     --    null_ls.builtins.formatting.prettier,
--- lua/options.lua
+++ lua/options.lua
@@ -20,58 +20,7 @@ vim.o.hidden = true
 vim.o.expandtab = true
 vim.o.encoding = "utf-8"
 vim.o.foldmethod = "indent"
-vim.o.foldlevelstart=99
-
--- vim.cmd([[
---   hi NeoTreeNormal guibg=0
---   hi NeoTreeNormalNC guibg=0
--- 
--- " Netrw 
--- let g:netrw_keepdir = 1
--- let g:netrw_altv = 1
--- let g:netrw_list_hide = '.*\.DS_Store,.\.vscode,node_modules'
--- let g:netrw_hide = 1       
--- let g:netrw_fastbrowse = 0 
--- let g:netrw_bufsettings = 'nonu noma nomod nobl nowrap ro'
--- let g:netrw_banner = 0
--- 
--- function! CopyFile()
---   let file = expand("%:p") . netrw#Call('NetrwGetWord')
---   let newfile = input('Copy file: ', file)
---   silent execute '!cp' file newfile
--- endfunction
--- 
--- hi! link netrwMarkFile Search
--- augroup netrw_mapping
---   autocmd!
---   autocmd filetype netrw call NetrwMapping()
--- augroup END
--- 
--- function! NetrwMapping() abort
---   setlocal nocursorline
---   nmap <buffer> <nowait> c :call CopyFile()<CR>
---   nmap <buffer> <nowait> o <CR>
---   nmap <buffer> <nowait> p -
---   nmap <buffer> <nowait> a %
---   nmap <buffer> <nowait> s v<C-h>
---   nmap <buffer> <nowait> . :bnext<CR>
---   nmap <buffer> <nowait> m :bprevious<CR>
--- endfunction
--- 
--- autocmd FileType netrw setl bufhidden=wipe
--- 
--- nnoremap <Leader>e :let @/=expand("%:t") <Bar> execute 'Explore' expand("%:h") <Bar> normal n<CR>
--- 
--- ]])
-
--- Configuración específica para el plugin "copilot.nvim"
---vim.cmd('let g:copilot_node_command = "~/node-v18.17.0/bin/node"')
--- vim.cmd([[
--- let g:copilot_filetypes = {
---   \ 'gitcommit': v:true,
---   \ 'markdown': v:true,
---   \ 'yaml': v:true
---   \ }
+vim.o.foldlevelstart = 99
 
 vim.cmd([[
 function! TerminalMappings()
