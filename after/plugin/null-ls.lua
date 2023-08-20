61032e1c5ec3634fce5b31697a50f96d0f8344bb
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-08-20 13:43:36 -0300

    fix

--- after/plugin/null-ls.lua
+++ after/plugin/null-ls.lua
@@ -5,27 +5,27 @@ if not null_ls_status_ok then
 end
 
 local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+local on_attach = function(_, bufnr)
+  if _.supports_method("textDocument/formatting") then
+    vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = augroup,
+      buffer = bufnr,
+      callback = function()
+        vim.lsp.buf.format({
+          bufnr = bufnr,
+          filter = function(client)
+             return client.name == "null-ls"
+          end,
+        })
+      end,
+    })
+  end
+end
+
 null_ls.setup({
   debug = false,
-  on_attach = function(_, bufnr)
-    if _.supports_method("textDocument/formatting") then
-      vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-      vim.api.nvim_create_autocmd("BufWritePre", {
-        group = augroup,
-        buffer = bufnr,
-        callback = function()
-          vim.lsp.buf.format({
-            bufnr = bufnr,
-            filter = function(client)
-              return client.name == "null-ls"
-            end,
-          })
-        end,
-      })
-    end
-  end,
-
-  -- the sources are prettier, eslint_d and rubocop
+  on_attach = on_attach,
   sources = {
  --   null_ls.builtins.formatting.stylua,
     null_ls.builtins.diagnostics.eslint,
