0f6d84e907893e76ea4ac11541548873b307f93a
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-09-10 01:00:20 -0300

    fix

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -1,3 +1,11 @@
+local opts = { noremap = true, silent = true }
+local on_attach = function(_, bufnr)
+	opts.buffer = bufnr
+
+	vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
+	vim.keymap.set("n", "L", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
+end
+
 return {
 	"neovim/nvim-lspconfig",
 	event = { "BufReadPre", "BufNewFile" },
@@ -12,14 +20,13 @@ return {
 
 		local capabilities = cmp_nvim_lsp.default_capabilities()
 
-		local opts = { noremap = true, silent = true }
-		local on_attach = function(_, bufnr)
-			opts.buffer = bufnr
+		vim.diagnostic.config({
+			virtual_text = false,
+		})
 
-			vim.keymap.set("n", "gf", "<cmd>lua vim.lsp.buf.definition()<CR>", opts)
-			vim.keymap.set("n", "L", "<cmd>lua vim.lsp.buf.hover()<CR>", opts)
-			--			vim.keymap.set("n", "<leader>a", vim.diagnostic.open_float, opts)
-		end
+		-- Show line diagnostics automatically in hover window
+		vim.o.updatetime = 250
+		vim.cmd([[autocmd CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]])
 
 		lspconfig["tsserver"].setup({
 			capabilities = capabilities,
