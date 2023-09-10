225fd63072f9a686631442f59fb440236aad748d
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-09-10 14:20:03 -0300

    feat: color scheme

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,16 @@
 return {
-	"folke/tokyonight.nvim",
+	--	"folke/tokyonight.nvim",
+	"shaunsingh/nord.nvim",
+	-- "catppuccin/nvim",
 	priority = 1000,
 	config = function()
-		vim.cmd.colorscheme("tokyonight-night")
+		-- vim.cmd.colorscheme("catppuccin")
+		-- vim.cmd.colorscheme("tokyonight-night")
+
+		vim.cmd.colorscheme("nord")
+		vim.g.nord_disable_background = true
+		vim.g.nord_italic = false
+		vim.g.nord_bold = true
+		require("nord").set()
 	end,
 }
