44f25af962a514e11fdfbed2ed92d9c52362e4ae
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-09-14 21:53:57 -0300

    fix

--- init.lua
+++ init.lua
@@ -1,23 +1,22 @@
-require("options")
-require("netrw")
-
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
-end
+-- if not vim.loop.fs_stat(lazypath) then
+-- 	vim.fn.system({
+-- 		"git",
+-- 		"clone",
+-- 		"--filter=blob:none",
+-- 		"https://github.com/folke/lazy.nvim.git",
+-- 		"--branch=stable",
+-- 		lazypath,
+-- 	})
+-- end
 vim.opt.rtp:prepend(lazypath)
 
-require("lazy").setup({
+local config = {
 	{ import = "plugins" },
 	{ import = "plugins.lsp" },
-}, {
+}
+
+local options = {
 	checker = {
 		enabled = true,
 		notify = false,
@@ -25,6 +24,9 @@ require("lazy").setup({
 	change_detection = {
 		notify = false,
 	},
-})
+}
 
-require("keymaps").setup()
+require("options")
+require("keymaps")
+require("netrw")
+require("lazy").setup(config, options)
--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -1,77 +1,63 @@
-local M = {}
-
-function M.setup()
-	function Quit()
-		local buffers = {}
-		for i = 1, vim.fn.bufnr("$") do
-			if vim.fn.buflisted(i) then
-				table.insert(buffers, i)
-			end
-		end
-
-		if #buffers == 1 then
-			vim.cmd("q")
-		else
-			vim.cmd("bd")
-		end
+function Quit()
+	if #vim.fn.getbufinfo({ buflisted = 1 }) == 1 then
+		vim.cmd("q")
+	else
+		vim.cmd("bd")
 	end
-
-	local nmap = function(keys, func)
-		vim.keymap.set("n", keys, func)
-	end
-
-	local imap = function(keys, func)
-		vim.keymap.set("i", keys, func)
-	end
-
-	local vmap = function(keys, func)
-		vim.keymap.set("v", keys, func)
-	end
-
-	nmap("<Leader>q", ":lua Quit()<CR>")
-	nmap("<Leader>p", ":Format<CR>")
-
-	imap("kj", "<Esc>")
-	nmap("<leader>w", ":w<CR>")
-
-	-- move between windows
-	nmap("<C-L>", "<C-W><C-L>")
-	nmap("<C-H>", "<C-W><C-H>")
-	imap("<C-L>", "<Esc><C-W><C-L>")
-	imap("<C-H>", "<Esc><C-W><C-H>")
-
-	-- auto close brackets
-	imap("(", "()<Esc>i")
-	imap("[", "[]<Esc>i")
-	imap("`", "``<Esc>i")
-	imap('"', '""<Esc>i')
-	imap("{", "{}<Esc>i")
-	imap("'", "''<Esc>i")
-
-	-- move lines up and down
-	nmap("<C-J>", "10j")
-	nmap("<C-K>", "10k")
-
-	-- move start and end of line
-	nmap("gl", "$")
-	nmap("gh", "0")
-	vmap("gl", "$")
-	vmap("gh", "0")
-
-	-- clean search
-	nmap("<leader><space>", ":noh<CR>")
-
-	-- move lines up and down
-	nmap("<S-j>", ":m .+1<CR>")
-	nmap("<S-k>", ":m .-2<CR>")
-
-	-- split buffer
-	nmap("s", ":vsp<CR>")
-
-	-- copy file path
-	nmap("cp", ':let @+ = expand("%") <bar> echo "copied " . expand("%")<CR>')
-
-	nmap("<leader>.", ":vsp .env<CR>")
 end
 
-return M
+local nmap = function(keys, func)
+	vim.keymap.set("n", keys, func)
+end
+
+local imap = function(keys, func)
+	vim.keymap.set("i", keys, func)
+end
+
+local vmap = function(keys, func)
+	vim.keymap.set("v", keys, func)
+end
+
+nmap("<Leader>q", ":lua Quit()<CR>")
+
+imap("kj", "<Esc>")
+nmap("<leader>w", ":w<CR>")
+
+-- move between windows
+nmap("<C-L>", "<C-W><C-L>")
+nmap("<C-H>", "<C-W><C-H>")
+imap("<C-L>", "<Esc><C-W><C-L>")
+imap("<C-H>", "<Esc><C-W><C-H>")
+
+-- auto close brackets
+imap("(", "()<Esc>i")
+imap("[", "[]<Esc>i")
+imap("`", "``<Esc>i")
+imap('"', '""<Esc>i')
+imap("{", "{}<Esc>i")
+imap("'", "''<Esc>i")
+
+-- move lines up and down
+nmap("<C-J>", "10j")
+nmap("<C-K>", "10k")
+
+-- move start and end of line
+nmap("gl", "$")
+nmap("gh", "0")
+vmap("gl", "$")
+vmap("gh", "0")
+
+-- clean search
+nmap("<leader><space>", ":noh<CR>")
+
+-- move lines up and down
+nmap("<S-j>", ":m .+1<CR>")
+nmap("<S-k>", ":m .-2<CR>")
+
+-- split buffer
+nmap("s", ":vsp<CR>")
+
+-- copy file path
+nmap("cp", ':let @+ = expand("%") <bar> echo "copied " . expand("%")<CR>')
+
+nmap("<leader>.", ":vsp .env<CR>")
