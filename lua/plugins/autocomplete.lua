1d8beccbfabd641adba865a3a2635d3098603776
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2023-10-07 17:14:50 -0300

    fix

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,31 +1,4 @@
 return {
-  'Exafunction/codeium.vim',
-  event = 'BufEnter'
-
-	--  'github/copilot.vim',
-	--	{
-	--		"zbirenbaum/copilot.lua",
-	--		cmd = "Copilot",
-	--		event = "InsertEnter",
-	--		config = function()
-	--			require("copilot").setup({
-	--				copilot_node_command = vim.fn.expand("$HOME") .. "/node-v18.17.0/bin/node",
-	--				suggestion = {
-	--					auto_trigger = true,
-	--					accept = false,
-	--				},
-	--				filetypes = {
-	--					gitcommit = true,
-	--				},
-	--			})
-	--
-	--			vim.keymap.set("i", "<Tab>", function()
-	--				if require("copilot.suggestion").is_visible() then
-	--					require("copilot.suggestion").accept()
-	--				else
-	--					vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<Tab>", true, false, true), "n", false)
-	--				end
-	--			end, { silent = true })
-	--		end,
-	--	},
+	"Exafunction/codeium.vim",
+	event = "BufEnter",
 }
