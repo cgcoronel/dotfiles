016529d712865c351772339433b585600ff2f72a
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-04-25 10:01:33 -0300

    fix

--- lua/options.lua
+++ lua/options.lua
@@ -25,3 +25,28 @@ local global_options = {
 for option, value in pairs(global_options) do
 	vim.o[option] = value
 end
+
+vim.cmd([[
+function! TerminalMappings()
+  startinsert
+  setlocal nonu nornu
+  tnoremap <buffer> <leader>q <C-\><C-n>:bd!<CR>
+  tnoremap <buffer> <c-h> <C-\><C-n><C-w><C-h>
+  tnoremap <buffer> <c-l> <C-\><C-n><C-w><C-l>
+endfunction
+
+autocmd TermOpen term://*zsh call TerminalMappings()
+autocmd TermOpen term://*npx* call TerminalMappings()
+autocmd BufEnter term://* startinsert
+]])
+
+vim.cmd([[
+command! Terminal call TermSplit()
+
+function! TermSplit()
+    vsplit 
+    term
+    call feedkeys("\<C-h>")
+    wincmd p
+endfunction
+]])
