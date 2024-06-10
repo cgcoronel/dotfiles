742a3c7f632e93813baa3a2e5d309ab324351da9
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-09 21:51:02 -0300

    fix

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,10 +1,16 @@
 return {
-	"Exafunction/codeium.nvim",
-	dependencies = {
-		"nvim-lua/plenary.nvim",
-		"hrsh7th/nvim-cmp",
+	{
+		"Exafunction/codeium.vim",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"hrsh7th/nvim-cmp",
+		},
+		commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
+		event = "BufEnter",
+		config = function()
+			vim.keymap.set("i", "<C-Space>", function()
+				return vim.fn["codeium#Accept"]()
+			end, { expr = true, silent = true })
+		end,
 	},
-	config = function()
-		require("codeium").setup({})
-	end,
 }
