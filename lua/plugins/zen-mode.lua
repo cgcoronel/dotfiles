57c64984209b310ed57097e917cb2754554f8e3e
Cristian Coronel <Cristian@ip-192-168-1-81.ec2.internal>
2024-06-17 11:41:29 -0300

    fix

---
+++ lua/plugins/zen-mode.lua
@@ -0,0 +1,16 @@
+return {
+	"folke/zen-mode.nvim",
+	opts = {
+		window = {
+			width = 0.50,
+		},
+		plugins = {
+			options = {
+				enabled = true,
+				ruler = true,
+				showcmd = true,
+				laststatus = 3,
+			},
+		},
+	},
+}
