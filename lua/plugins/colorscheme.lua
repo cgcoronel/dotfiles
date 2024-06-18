0ca2d8a33620ccceb52a54c5ac28f4231796f67d
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-18 10:17:51 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,24 +1,24 @@
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = function()
--- 		return {
--- 			transparent = true,
--- 		}
--- 	end,
--- 	config = function()
--- 		vim.cmd("colorscheme tokyonight-night")
--- 	end,
--- }
-
 return {
-	"eldritch-theme/eldritch.nvim",
+	"folke/tokyonight.nvim",
 	lazy = false,
 	priority = 1000,
-	opts = {},
+	opts = function()
+		return {
+			transparent = true,
+		}
+	end,
 	config = function()
-		vim.cmd("colorscheme eldritch")
-    vim.cmd("hi NormalFloat guifg=#ebfafa guibg=#171928")
+		vim.cmd("colorscheme tokyonight-night")
 	end,
 }
+
+-- return {
+-- 	"eldritch-theme/eldritch.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	opts = {},
+-- 	config = function()
+-- 		vim.cmd("colorscheme eldritch")
+--     vim.cmd("hi NormalFloat guifg=#ebfafa guibg=#171928")
+-- 	end,
+-- }
