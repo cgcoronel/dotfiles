98cae6b0fd719656eb7be5b4f8d8df81d69c0cc6
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-22 20:54:24 -0300

    fix

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,92 +1,92 @@
 return {
---	"nvim-lualine/lualine.nvim",
---	dependencies = { "nvim-tree/nvim-web-devicons" },
---	init = function()
---		require("lualine").setup({
---			options = {
---				icons_enabled = true,
---				theme = "auto",
---				component_separators = { left = "", right = "" },
---				section_separators = { left = "", right = "" },
---				refresh = {
---					statusline = 1000,
---					tabline = 1000,
---					winbar = 1000,
---				},
---			},
---			sections = {
---				lualine_a = {
---					{
---						"filetype",
---						color = { bg = "#0d1117", fg = "#e6eaea" },
---						--						color = { bg = "#1a1b26" }, -- tokyonight
---						icon_only = true,
---					},
---					{
---						"filename",
---						--						color = { bg = "#1a1b26", fg = "#e6eaea" }, -- tokyonight
---						color = { bg = "#0d1117", fg = "#e6eaea" },
---						file_status = false,
---						path = 1,
---					},
---				},
---				lualine_b = {
---					{
---						"diagnostics",
---					},
---				},
---				lualine_c = {
---					{
---						function()
---							return ""
---						end,
---						color = { bg = "#0d1117" },
---					},
---				},
---				lualine_x = {},
---				lualine_y = {
---					{
---						"location",
---						--						color = { bg = "#1a1b26" }, -- tokyonight
---					},
---				},
---				lualine_z = {},
---			},
---			inactive_sections = {
---				lualine_a = {
---					{
---						"filetype",
---						color = { bg = "#0d1117", fg = "#e6eaea" },
---						icon_only = true,
---					},
---					{
---						"filename",
---						color = { bg = "#0d1117", fg = "#e6eaea" },
---						file_status = false,
---						path = 1,
---					},
---				},
---				lualine_b = {
---					{
---						"diagnostics",
---					},
---				},
---				lualine_c = {
---					{
---						function()
---							return ""
---						end,
---						color = { bg = "#0d1117" },
---					},
---				},
---				lualine_x = {},
---				lualine_y = {
---					{
---						"location",
---					},
---				},
---				lualine_z = {},
---			},
---		})
-	end,
+	--	"nvim-lualine/lualine.nvim",
+	--	dependencies = { "nvim-tree/nvim-web-devicons" },
+	--	init = function()
+	--		require("lualine").setup({
+	--			options = {
+	--				icons_enabled = true,
+	--				theme = "auto",
+	--				component_separators = { left = "", right = "" },
+	--				section_separators = { left = "", right = "" },
+	--				refresh = {
+	--					statusline = 1000,
+	--					tabline = 1000,
+	--					winbar = 1000,
+	--				},
+	--			},
+	--			sections = {
+	--				lualine_a = {
+	--					{
+	--						"filetype",
+	--						color = { bg = "#0d1117", fg = "#e6eaea" },
+	--						--						color = { bg = "#1a1b26" }, -- tokyonight
+	--						icon_only = true,
+	--					},
+	--					{
+	--						"filename",
+	--						--						color = { bg = "#1a1b26", fg = "#e6eaea" }, -- tokyonight
+	--						color = { bg = "#0d1117", fg = "#e6eaea" },
+	--						file_status = false,
+	--						path = 1,
+	--					},
+	--				},
+	--				lualine_b = {
+	--					{
+	--						"diagnostics",
+	--					},
+	--				},
+	--				lualine_c = {
+	--					{
+	--						function()
+	--							return ""
+	--						end,
+	--						color = { bg = "#0d1117" },
+	--					},
+	--				},
+	--				lualine_x = {},
+	--				lualine_y = {
+	--					{
+	--						"location",
+	--						--						color = { bg = "#1a1b26" }, -- tokyonight
+	--					},
+	--				},
+	--				lualine_z = {},
+	--			},
+	--			inactive_sections = {
+	--				lualine_a = {
+	--					{
+	--						"filetype",
+	--						color = { bg = "#0d1117", fg = "#e6eaea" },
+	--						icon_only = true,
+	--					},
+	--					{
+	--						"filename",
+	--						color = { bg = "#0d1117", fg = "#e6eaea" },
+	--						file_status = false,
+	--						path = 1,
+	--					},
+	--				},
+	--				lualine_b = {
+	--					{
+	--						"diagnostics",
+	--					},
+	--				},
+	--				lualine_c = {
+	--					{
+	--						function()
+	--							return ""
+	--						end,
+	--						color = { bg = "#0d1117" },
+	--					},
+	--				},
+	--				lualine_x = {},
+	--				lualine_y = {
+	--					{
+	--						"location",
+	--					},
+	--				},
+	--				lualine_z = {},
+	--			},
+	--		})
+	--	end,
 }
