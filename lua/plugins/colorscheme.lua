aecf7e6fdee4797251a180c9b955277251ff58f2
Cristian Coronel <cristiancoronel@Cristians-MBP.fibertel.com.ar>
2024-06-22 17:05:02 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,16 +1,16 @@
-return {
-	"folke/tokyonight.nvim",
-	lazy = false,
-	priority = 1000,
-	opts = function()
-		return {
-			transparent = true,
-		}
-	end,
-	config = function()
-		vim.cmd("colorscheme tokyonight-night")
-	end,
-}
+-- return {
+-- 	"folke/tokyonight.nvim",
+-- 	lazy = false,
+-- 	priority = 1000,
+-- 	opts = function()
+-- 		return {
+-- 			transparent = true,
+-- 		}
+-- 	end,
+-- 	config = function()
+-- 		vim.cmd("colorscheme tokyonight-night")
+-- 	end,
+-- }
 
 -- return {
 -- 	"eldritch-theme/eldritch.nvim",
@@ -22,3 +22,21 @@ return {
 --     vim.cmd("hi NormalFloat guifg=#ebfafa guibg=#171928")
 -- 	end,
 -- }
+
+return {
+	"projekt0n/github-nvim-theme",
+	lazy = false,
+	priority = 1000,
+	config = function()
+		require("github-theme").setup({
+			options = {
+				styles = {
+					comments = "italic",
+					keywords = "bold",
+					types = "italic,bold",
+				},
+			},
+		})
+		vim.cmd("colorscheme github_dark_tritanopia")
+	end,
+}
