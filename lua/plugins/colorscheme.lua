4b51d27d1d6763541b8a6723ef10ee38c74adeba
Cristian Coronel <Cristian@ip-192-168-1-81.ec2.internal>
2024-06-24 18:15:33 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -35,6 +35,12 @@ return {
 		  hi EndOfBuffer guifg=#1a1a25 
       hi WinSeparator guibg=#171b21 guifg=#333a43
       hi LineNr guibg=#171b21 
+
+      hi SignColumn guibg=none
+      hi GitSignsAdd guibg=none 
+      hi GitSignsChange guibg=none 
+      hi GitSignsDelete guibg=none 
+
 		]])
 	end,
 }
