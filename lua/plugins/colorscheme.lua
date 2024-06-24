ab49788c90fc5aaa3c4eacf09315ef6216085dbc
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-24 19:57:15 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,28 +1,3 @@
--- return {
--- 	"folke/tokyonight.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = function()
--- 		return {
--- 			transparent = true,
--- 		}
--- 	end,
--- 	config = function()
--- 		vim.cmd("colorscheme tokyonight-night")
--- 	end,
--- }
-
--- return {
--- 	"eldritch-theme/eldritch.nvim",
--- 	lazy = false,
--- 	priority = 1000,
--- 	opts = {},
--- 	config = function()
--- 		vim.cmd("colorscheme eldritch")
---     vim.cmd("hi NormalFloat guifg=#ebfafa guibg=#171928")
--- 	end,
--- }
-
 return {
 	"Yazeed1s/minimal.nvim",
 	lazy = false,
@@ -35,7 +10,6 @@ return {
 		  hi EndOfBuffer guifg=#1a1a25 
       hi WinSeparator guibg=#171b21 guifg=#333a43
       hi LineNr guibg=#171b21 
-
       hi SignColumn guibg=none
       hi GitSignsAdd guibg=none 
       hi GitSignsChange guibg=none 
@@ -43,39 +17,49 @@ return {
       hi DiagnosticSignHint guibg=none
       hi DiagnosticSignError guibg=none
       hi DiagnosticSignWarn guibg=none
+      hi GitSignsStagedDelete guibg=none
+      hi GitSignsStagedAdd guibg=none
+      hi GitSignsStagedTopdelete guibg=none
 		]])
 	end,
 }
 
--- return {
--- 	"projekt0n/github-nvim-theme",
--- 	lazy = false,
--- 	priority = 1000,
--- 	config = function()
--- 		require("github-theme").setup({
---
--- 			groups = {
--- 				all = {
--- 					Normal = { bg = "#1e2129" },
--- 					NormalNC = { bg = "#1e2129" },
--- 				},
--- 			},
---
--- 			options = {
--- 				transparent = false,
--- 				styles = {
--- 					comments = "italic",
--- 					keywords = "bold",
--- 					types = "italic,bold",
--- 				},
--- 			},
--- 		})
---
--- 		vim.cmd("colorscheme github_dark_tritanopia")
--- 		vim.cmd([[
---       hi EndOfBuffer guifg=#1e2129
---       hi StatusLine guifg=#c9d1d9 guibg=#0e1116
---       hi StatusLineNC cterm=NONE gui=NONE guifg=#30363d guibg=#0e1116
---     ]])
--- 	end,
--- }
+
+
+
+-- hi GitSignsChangedelete guibg=none
+-- hi GitSignsTopdelete guibg=none
+-- hi GitSignsUntracked guibg=none
+-- hi GitSignsAddNr guibg=none
+-- hi GitSignsChangeNr guibg=none
+-- hi GitSignsDeleteNr guibg=none
+-- hi GitSignsChangedeleteNr guibg=none
+-- hi GitSignsTopdeleteNr guibg=none
+-- hi GitSignsUntrackedNr guibg=none
+-- hi GitSignsAddLn guibg=none
+-- hi GitSignsChangeLn guibg=none
+-- hi GitSignsChangedeleteLn guibg=none
+-- hi GitSignsUntrackedLn guibg=none
+-- hi GitSignsStagedChange guibg=none
+-- hi GitSignsStagedChangedelete guibg=none
+-- hi GitSignsStagedTopdelete guibg=none
+-- hi GitSignsStagedAddNr guibg=none
+-- hi GitSignsStagedChangeNr guibg=none
+-- hi GitSignsStagedDeleteNr guibg=none
+-- hi GitSignsStagedChangedeleteNr guibg=none
+-- hi GitSignsStagedTopdeleteNr guibg=none
+-- hi GitSignsStagedAddLn guibg=none
+-- hi GitSignsStagedChangeLn guibg=none
+-- hi GitSignsStagedChangedeleteLn guibg=none
+-- hi GitSignsAddPreview guibg=none
+-- hi GitSignsDeletePreview guibg=none
+-- hi GitSignsCurrentLineBlame guibg=none
+-- hi GitSignsAddInline guibg=none
+-- hi GitSignsDeleteInline guibg=none
+-- hi GitSignsChangeInline guibg=none
+-- hi GitSignsAddLnInline guibg=none
+-- hi GitSignsChangeLnInline guibg=none
+-- hi GitSignsDeleteLnInline guibg=none
+-- hi GitSignsDeleteVirtLn guibg=none
+-- hi GitSignsDeleteVirtLnInLine guibg=none
+-- hi GitSignsVirtLnum guibg=none
