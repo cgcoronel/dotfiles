4fb64329de6035df88084e3c119865ceac651fa5
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-26 23:55:11 -0300

    fix

---
+++ lua/plugins/noice.lua
@@ -0,0 +1,26 @@
+return {
+	"folke/noice.nvim",
+	event = "VeryLazy",
+	opts = {},
+	dependencies = {
+		"MunifTanjim/nui.nvim",
+		"rcarriga/nvim-notify",
+	},
+	config = function()
+		require("noice").setup({
+			messages = {
+				enabled = false, 
+			},
+			presets = {
+--				bottom_search = true,
+				command_palette = true,
+			},
+		})
+		vim.cmd([[
+      hi NoiceCmdlinePopupBorder guifg=#333a43 guibg=none
+      hi NoiceCmdlineIconCmdline guifg=#333a43 guibg=none
+      hi NoiceCmdlineIconSearch guifg=#333a43 guibg=none
+      hi NoiceCmdlinePopupBorderSearch guifg=#333a43 guibg=none
+    ]])
+	end,
+}
