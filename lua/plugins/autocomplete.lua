2a6b7710bcd2203691dbf1a8362be1606f211301
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-06-28 13:03:26 -0300

    fix

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -5,7 +5,7 @@ return {
 			"nvim-lua/plenary.nvim",
 			"hrsh7th/nvim-cmp",
 		},
-		commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
+		--		commit = "289eb724e5d6fab2263e94a1ad6e54afebefafb2",
 		event = "BufEnter",
 		config = function()
 			vim.keymap.set("i", "<C-Space>", function()
