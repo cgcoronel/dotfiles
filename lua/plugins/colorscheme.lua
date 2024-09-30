a27ff315006488dd16a94352a3ee1b48385c4504
Cristian Coronel <Cristian@cristiapelincom.fibertel.com.ar>
2024-09-30 11:18:42 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -7,20 +7,20 @@ return {
 		vim.cmd("colorscheme minimal")
 
 		vim.cmd([[
- 		  hi EndOfBuffer guifg=#1a1a25
-       hi WinSeparator guibg=#171b21 guifg=#333a43
-       hi LineNr guibg=#171b21
-       hi SignColumn guibg=none
-       hi GitSignsAdd guibg=none
-       hi GitSignsChange guibg=none
-       hi GitSignsDelete guibg=none
-       hi DiagnosticSignHint guibg=none
-       hi DiagnosticSignError guibg=none
-       hi DiagnosticSignWarn guibg=none
-       hi GitSignsStagedDelete guibg=none
-       hi GitSignsStagedAdd guibg=none
-       hi GitSignsStagedTopdelete guibg=none
- 		]])
+		  hi EndOfBuffer guifg=#1a1a25
+      hi WinSeparator guibg=#171b21 guifg=#333a43
+      hi LineNr guibg=#171b21
+      hi SignColumn guibg=none
+      hi GitSignsAdd guibg=none
+      hi GitSignsChange guibg=none
+      hi GitSignsDelete guibg=none
+      hi DiagnosticSignHint guibg=none
+      hi DiagnosticSignError guibg=none
+      hi DiagnosticSignWarn guibg=none
+      hi GitSignsStagedDelete guibg=none
+      hi GitSignsStagedAdd guibg=none
+      hi GitSignsStagedTopdelete guibg=none
+		]])
 	end,
 }
 
@@ -39,3 +39,4 @@ return {
 --     vim.cmd([[ colorscheme neofusion ]])
 --  	end,
 -- }
+
