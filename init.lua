e92c746847243d4d7de77c2d01b54031a2f38e24
Cristian Coronel <Cristian@ip-10-228-1-244.ec2.internal>
2024-12-09 13:26:01 -0300

    fix: colors

--- init.lua
+++ init.lua
@@ -36,8 +36,9 @@ require("lazy").setup(plugins, options)
 vim.api.nvim_create_autocmd("User", {
 	pattern = "LazyVimStarted",
 	callback = function()
+		--      colorscheme xcode
 		vim.cmd([[
-      colorscheme xcode
+        colorscheme xcodedarkhc
        hi StatusLine guifg=#838991 guibg=#34353b
        hi WinSeparator guibg=none guifg=#52555c
      ]])
