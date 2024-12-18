2505b07f76a8d18875d439a0dcf67d212dc2cf42
Cristian Coronel <Cristian@ip-192-168-1-18.ec2.internal>
2024-12-18 17:53:57 -0300

    fix

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -6,6 +6,11 @@ return {
 	"rose-pine/neovim",
 	name = "rose-pine",
 	config = function()
+		require("rose-pine").setup({
+			styles = {
+				transparency = true,
+			},
+		})
 		vim.cmd("colorscheme rose-pine")
 	end,
 }
