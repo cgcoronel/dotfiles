a169b005bb4f996dc74d5cac489b9ecb1babcc59
Cristian Coronel <Cristian@cristianxepelincom.local>
2024-12-21 10:49:41 -0300

    fix

--- lua/plugins/autocomplete.lua
+++ lua/plugins/autocomplete.lua
@@ -1,15 +1,18 @@
 return {
+	--	{
+	--		"Exafunction/codeium.vim",
+	--		dependencies = {
+	--			"nvim-lua/plenary.nvim",
+	--			"hrsh7th/nvim-cmp",
+	--		},
+	--		event = "BufEnter",
+	--		config = function()
+	--			vim.keymap.set("i", "<C-Space>", function()
+	--				return vim.fn["codeium#Accept"]()
+	--			end, { expr = true, silent = true })
+	--		end,
+	--	},
 	{
-		"Exafunction/codeium.vim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		event = "BufEnter",
-		config = function()
-			vim.keymap.set("i", "<C-Space>", function()
-				return vim.fn["codeium#Accept"]()
-			end, { expr = true, silent = true })
-		end,
+		"github/copilot.vim",
 	},
 }
