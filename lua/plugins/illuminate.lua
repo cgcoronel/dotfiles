87afc4a3af558cb30c0fa77a2a24cb2b775d7c1a
Cristian Coronel <Cristian@cristianxepelincom.local>
2025-04-11 15:56:09 -0300

    fix

--- lua/plugins/illuminate.lua
+++ lua/plugins/illuminate.lua
@@ -1,27 +1,27 @@
 return {
-	"RRethy/vim-illuminate",
-	event = { "BufReadPost", "BufNewFile" },
-	opts = {
-		delay = 200,
-		large_file_cutoff = 2000,
-		large_file_overrides = {
-			providers = { "lsp" },
-		},
-	},
-	config = function(_, opts)
-		require("illuminate").configure(opts)
-
-		local function map(key, dir, buffer)
-			vim.keymap.set("n", key, function()
-				require("illuminate")["goto_" .. dir .. "_reference"](false)
-			end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
-		end
-
-		map(";", "next")
-		map("<C-;>", "prev")
-
-		vim.cmd([[
-	       hi IlluminatedWordRead gui=none guifg=none guibg=#272932
-	     ]])
-	end,
+--	"RRethy/vim-illuminate",
+--	event = { "BufReadPost", "BufNewFile" },
+--	opts = {
+--		delay = 200,
+--		large_file_cutoff = 2000,
+--		large_file_overrides = {
+--			providers = { "lsp" },
+--		},
+--	},
+--	config = function(_, opts)
+--		require("illuminate").configure(opts)
+--
+--		local function map(key, dir, buffer)
+--			vim.keymap.set("n", key, function()
+--				require("illuminate")["goto_" .. dir .. "_reference"](false)
+--			end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+--		end
+--
+--		map(";", "next")
+--		map("<C-;>", "prev")
+--
+--		vim.cmd([[
+--	       hi IlluminatedWordRead gui=none guifg=none guibg=#272932
+--	     ]])
+--	end,
 }
