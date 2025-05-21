cd3b2dacdd7e43531a5c4953026ed9a76fbdbdbb
Cristian Coronel <Cristian@Mac.fibertel.com.ar>
2025-05-21 10:46:01 -0300

    fic

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -19,9 +19,7 @@ return {
 			},
 		},
 	},
-	-- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
 	build = "make",
-	-- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
 	dependencies = {
 		"nvim-treesitter/nvim-treesitter",
 		"stevearc/dressing.nvim",
